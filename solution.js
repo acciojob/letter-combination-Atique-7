936281b51f889b3426e9b277fc0ad90e633873fb
ATIQUE KHAN <80064685+Atique-7@users.noreply.github.com>
2022-12-26 17:40:39 +0530

    backtrack

--- solution.js
+++ solution.js
@@ -1,5 +1,34 @@
 function letterCombinations(input_digit) {
   //Complete the function
+  let res = [];
+  
+  let dict = {
+     "2" : "abc",
+     "3" : "def",
+     "4" : "ghi",
+     "5" : "jkl",
+     "6" : "mno",
+     "7" : "pqrs",
+     "8" : "tuv",
+     "9" : "wxyz"
+  };
+  
+  function backtrack(i, curStr) {
+    if(curStr.length == digits.length){
+      res.push(curStr);
+      return;
+    }
+    
+    for(let c of dict[input_digits[i]) {
+      backtrack(i+1, curStr + c);
+    }
+  }
+  
+  if(input_digits){
+    backtrack(0,"");
+  }
+  
+  return res;
 }
 
 module.exports = letterCombinations;
